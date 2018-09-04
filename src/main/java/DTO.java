00a94e8f81111a4f8b884a662dd8a89920d97459
Song <tjgus2932@gmail.com>
2018-09-04 20:35:35 +0900

    로또 생성

---
+++ src/main/java/DTO.java
@@ -0,0 +1,12 @@
+
+public class DTO {
+    private int firstBall;
+    private int secondBall;
+    private int thirdBall;
+    private int fouthBall;
+    private int fifthBall;
+    private int sixthBall;
+    
+    
+
+}
